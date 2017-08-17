c70183134471c7747083d80ff6a36874bb416d11
Nigel Finley <nfinleymusic@gmail.com>
2017-08-17 00:20:45 -0500

    incorporated a few bootstrap-vue components

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,8 +1,57 @@
 <template>
   <div class="hello">
+    <b-navbar toggleable type="light" variant="info" toggle-breakpoint="md">
+
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-nav is-nav-bar>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-nav>
+
+        <!-- Right alignd nav items -->
+        <b-nav is-nav-bar class="ml-auto">
+
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item to="#">EN</b-dropdown-item>
+            <b-dropdown-item to="#">ES</b-dropdown-item>
+            <b-dropdown-item to="#">RU</b-dropdown-item>
+            <b-dropdown-item to="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item to="#">Profile</b-dropdown-item>
+            <b-dropdown-item to="#">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-nav>
+
+      </b-collapse>
+    </b-navbar>
+
+    <b-jumbotron header="ThruDerm" lead="Over the counter Transdermal Topical Analgesics" >
+      <p>Have Muscle Pain?</p>
+      <b-btn variant="primary" href="#">Contact Us</b-btn>
+    </b-jumbotron>
+
+
+<!--
     <div class="header-image">
       <img src="../assets/fake_cover.png" class="cover-img">
     </div>
+-->
 
     <h1>{{ msg }}</h1>
 
